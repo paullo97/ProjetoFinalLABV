8ff1ed2029734075520d7921e9085f39af8090ad
paullo97 <paulo.cesar97@hotmail.com>
2019-05-10 21:51:08 -0300

    Alteracao de arquivos

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div class="title">Trabalho final - Laboratório de Desenvolvimento em Banco de Dados V</div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    
+    <p class="intro">Acesso</p>
+    <input type="text" placeholder="CPF"><br>
+    <input type="text" placeholder="CODIGO"><br>
+    <button class="Acessar">Entrar</button>
+
+    <p class="Cadastro">Não tem cadastro? <a href="#">Criar Cadastro</a></p>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  
+  .intro{font-size: 2em; }
+  .Cadastro{font-size: 1em; margin-left: 80%;}
+  .title{font-size: 1.5em; background-color: rgba(0, 124, 128, 0.644); padding: 1%; height: auto; width: auto}
+  input{ margin-top: 1em; height: 2em; width: 20%; border-radius: 5px; box-shadow: 2px 3px #333333; }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .Acessar{margin-top: 2em; height: 2em; width: 10%; font-size: 1.5vw; background-color: rgb(12, 184, 98); color: white; border-radius: 10px; overflow: hidden;}
 </style>
